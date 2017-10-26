1487e5ed7c4fe833112850e39a2c1442f9a49704
Ferg <austen.a.ferguson@gmail.com>
2017-10-26 19:57:00 -0400

    Setting up remote repository for java program that will eventually encrypt text.

---
+++ BUILD
@@ -0,0 +1,5 @@
+java_binary(
+    name = "CaesarCipher",
+    main_class = "org.thatguyferg.CaesarCipher",
+    srcs = glob(["src/main/java/org/thatguyferg/*.java"]),
+)
---
+++ src/main/java/org/thatguyferg/CaesarCipher.java
@@ -0,0 +1,9 @@
+package org.thatguyferg;
+
+public class CaesarCipher {
+
+    public static void main(String [] args){
+        System.out.println("Hello world");
+    }
+
+}
