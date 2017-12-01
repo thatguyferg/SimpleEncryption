c93e5f7ca342a549581dcec18568d9a371771420
Ferg <austen.a.ferguson@gmail.com>
2017-11-30 20:22:55 -0500

    Working Caesar Cipher encryption.

--- src/main/java/org/thatguyferg/CaesarCipher.java
+++ src/main/java/org/thatguyferg/CaesarCipher.java
@@ -1,9 +1,20 @@
 package org.thatguyferg;
 
+import java.util.stream.Collectors;
+
 public class CaesarCipher {
 
-    public static void main(String [] args){
-        System.out.println("Hello world");
+    final private int shift;
+
+    public CaesarCipher(final int shift) {
+      this.shift = shift;
     }
 
+    public String encrypt(final String s){
+      return s.toUpperCase()
+      .chars()
+      .boxed()
+      .map(c -> String.valueOf((char)((((c.intValue()-65)+shift)%26)+65)))
+      .collect(Collectors.joining());
+    }
 }
--- src/test/java/org/thatguyferg/CaesarCipherTest.java
+++ src/test/java/org/thatguyferg/CaesarCipherTest.java
@@ -1,11 +1,14 @@
 package org.thatguyferg;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class CaesarCipherTest {
 
   @Test
   public void doesitWork() {
-
+    CaesarCipher cipher = new CaesarCipher(4);
+    assertEquals("HSK", cipher.encrypt("dog"));
   }
 }
